1b7c6efc7797dad36301e020edc99eb804862fb0
Andrew Scott <ascott18@gmail.com>
2011-10-10 22:44:10 +0000

    Added support for LovelyLoot.
    git-svn-id: https://repos.curseforge.com/wow/improved-loot-frame/trunk@14 6a9c5906-dfc1-4108-835e-bb3d7a10f307

--- ImprovedLootFrame.lua
+++ ImprovedLootFrame.lua
@@ -4,37 +4,41 @@
 -- By Cybeloras of Mal'Ganis
 -- --------------------------
 
+local LovelyLootLoaded = IsAddOnLoaded("LovelyLoot")
+
 -- Woah, nice coding, blizz. Anchor something positioned at the top of the frame to the center of the frame instead, and make it an anonymous font string so I have to work to find it
 local i, t = 1, "Interface\\LootFrame\\UI-LootPanel"
+if not LovelyLootLoaded then
 while true do
-    local r = select(i, LootFrame:GetRegions())
+	local r = select(i, LootFrame:GetRegions())
 	if not r then break end
-    if r.GetText and r:GetText() == ITEMS then
-        r:ClearAllPoints()
-        r:SetPoint("TOP", -12, -19.5)
-    elseif r.GetTexture and r:GetTexture() == t then
-        r:Hide()
-    end
+	if r.GetText and r:GetText() == ITEMS then
+		r:ClearAllPoints()
+		r:SetPoint("TOP", -12, -19.5)
+	elseif r.GetTexture and r:GetTexture() == t then
+		r:Hide()
+	end
 	i = i + 1
 end
 
-local top = LootFrame:CreateTexture("LootFrameBackdropTop")
-top:SetTexture(t)
-top:SetTexCoord(0, 1, 0, 0.3046875)
-top:SetPoint("TOP")
-top:SetHeight(78)
+	local top = LootFrame:CreateTexture("LootFrameBackdropTop")
+	top:SetTexture(t)
+	top:SetTexCoord(0, 1, 0, 0.3046875)
+	top:SetPoint("TOP")
+	top:SetHeight(78)
 
-local bottom = LootFrame:CreateTexture("LootFrameBackdropBottom")
-bottom:SetTexture(t)
-bottom:SetTexCoord(0, 1, 0.9296875, 1)
-bottom:SetPoint("BOTTOM")
-bottom:SetHeight(18)
+	local bottom = LootFrame:CreateTexture("LootFrameBackdropBottom")
+	bottom:SetTexture(t)
+	bottom:SetTexCoord(0, 1, 0.9296875, 1)
+	bottom:SetPoint("BOTTOM")
+	bottom:SetHeight(18)
 
-local mid = LootFrame:CreateTexture("LootFrameBackdropMiddle")
-mid:SetTexture(t)
-mid:SetTexCoord(0, 1, 0.3046875, 0.9296875)
-mid:SetPoint("TOP", top, "BOTTOM")
-mid:SetPoint("BOTTOM", bottom, "TOP")
+	local mid = LootFrame:CreateTexture("LootFrameBackdropMiddle")
+	mid:SetTexture(t)
+	mid:SetTexCoord(0, 1, 0.3046875, 0.9296875)
+	mid:SetPoint("TOP", top, "BOTTOM")
+	mid:SetPoint("BOTTOM", bottom, "TOP")
+end
 
 local p, r, x, y = "TOP", "BOTTOM", 0, -4
 local buttonHeight = LootButton1:GetHeight() + abs(y)
@@ -42,14 +46,24 @@ local baseHeight = LootFrame:GetHeight() - (buttonHeight * LOOTFRAME_NUMBUTTONS)
 
 local old_LootFrame_Show = LootFrame_Show
 function LootFrame_Show(self, ...)
-    LootFrame:SetHeight(baseHeight + (GetNumLootItems() * buttonHeight))
-    for i = LOOTFRAME_NUMBUTTONS+1, GetNumLootItems() do
-		if not _G["LootButton"..i] then
-			CreateFrame("Button", "LootButton"..i, LootFrame, "LootButtonTemplate", i):SetPoint(p, "LootButton"..(i-1), r, x, y)
+	LootFrame:SetHeight(baseHeight + (GetNumLootItems() * buttonHeight))
+	local num = GetNumLootItems()
+	for i = 1, GetNumLootItems() do
+		if i > LOOTFRAME_NUMBUTTONS then
+			local button = _G["LootButton"..i]
+			if not button then
+				button = CreateFrame("Button", "LootButton"..i, LootFrame, "LootButtonTemplate", i)
+			end
+			LOOTFRAME_NUMBUTTONS = i
 		end
-        LOOTFRAME_NUMBUTTONS = i
-    end
-    return old_LootFrame_Show(self, ...)
+		if i > 1 then
+			local button = _G["LootButton"..i]
+			button:ClearAllPoints()
+			button:SetPoint(p, "LootButton"..(i-1), r, x, y)
+		end
+	end
+	
+	return old_LootFrame_Show(self, ...)
 end
 
 -- the following is inspired by http://us.battle.net/wow/en/forum/topic/2353268564 and is hacktastic
